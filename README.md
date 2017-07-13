3e3157c89fe7e41ae94a450f70e6940b5098c7c5
github username <github email address>
2017-07-13 13:00:25 -0400

    finish

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Using the app
 
-- Fork and clone this repo
+- Clone this repo
 - In your terminal run: $ npm install
 - In your terminal run: $ npm start
 - Open http://localhost:3000 to view the calendar in the browser
--- src/Event.js
+++ src/Event.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { Table } from 'semantic-ui-react'
 
 function Event(props){
-  let numRows = (parseFloat(props.event.endTime.replace(':', '.')) - parseFloat(props.event.startTime.replace(':', '.').replace('3', '5'))) * 2
-  console.log(numRows)
+  let startHour = props.event.startTime.split(':')[0]
+  let fractionOfStartHour =  props.event.startTime.split(':')[1].replace('3', '5')
+  let newStart = startHour + "." + fractionOfStartHour
+
+  let endHour = props.event.endTime.split(':')[0]
+  let fractionOfEndHour =  props.event.endTime.split(':')[1].replace('3', '5')
+  let newEnd = endHour + "." + fractionOfEndHour
+
+  let numRows = (parseFloat(newEnd) - parseFloat(newStart)) * 2
 
   return(
     <Table.Cell rowSpan={numRows} className='event'>
